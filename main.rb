b87013d3f5ba1bdd578a8c5987591bade6c7f577
Kudzai Nyandoro <kudzain@gmail.com>
2014-11-27 05:40:15 -0800

    Add calculate total helper.

--- main.rb
+++ main.rb
@@ -5,6 +5,35 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => 'kjhfiebfcnvbmzfalfhjfhxcbmzbvwruquhqqer' 
 
+helpers do
+  # Cards is [['S', '9'], ['C', 'Q']] (i.e. nested arrays)
+  # Curned into an array of values in the next line.
+  def calculate_total(cards)
+    arr = cards.map{|element| element[1]}
+
+    total = 0
+    # Iterate through array of values only. 
+    arr.each do |a|
+      if a == "A"
+        total += 11
+      else
+        # When to_i is called on a non integer, it is evaluated to 0.
+        # So 'J', 'Q', 'K' in this case will be evaluated to 0.
+        total += a.to_i == 0 ? 10 : a.to_i 
+      end
+  end
+
+    #Correct for Aces
+    # First select Ace values, count the number of times that 
+    # -- Ace occurrs.
+    arr.select{|element| element == "A"}.count.times do 
+      break if total <= 21 
+      total -= 10 
+    end
+    total
+  end
+end
+
 get '/' do 
   if session[:player_name]
     redirect '/game'
@@ -27,15 +56,15 @@ get '/game' do
   suits = ['H', 'D', 'C', 'S']
   values = ['2', '3', '4', '5', '6', '7', '8', '9', '9', 'A']
   session[:deck] = suits.product(values).shuffle
+
   # deal cards
-  #   dealer cards
   session[:dealer_cards] = []
   session[:player_cards] = []
   session[:dealer_cards] << session[:deck].pop
   session[:player_cards] << session[:deck].pop
   session[:dealer_cards] << session[:deck].pop
   session[:player_cards] << session[:deck].pop
-  #   player cards
+ 
 
 
 erb :game 
